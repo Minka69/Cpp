7bfadf1cf8352547c8096e925f64b8d3291e54ab
Mark Shislov <115359051+Minka69@users.noreply.github.com>
2024-10-15 11:05:03 +0300

    Update User_greeting.cpp
    added some lines to not close terminal automatically after code executes to force user to press "ENTER" to close the terminal

--- User_greeting.cpp
+++ User_greeting.cpp
@@ -6,20 +6,20 @@ int main() {
     int age;
     int year;
 
-    // Ask for the user's name
     std::cout << "Enter your name: ";
     std::getline(std::cin, name);
 
-    // Ask for the user's age
     std::cout << "Enter your age: ";
     std::cin >> age;
 
-    // Ask for the user's year of birth
     std::cout << "What year you were born in: ";
     std::cin >> year;
 
-    // Greet the user and mention their age
-    std::cout << "Hello, " << name << "! You are " << age << " years old and you were born in " << year << std::endl;
+    std::cout << "Hello, " << name << "! You are " << age << " years old and you were born in " << year << "!" << std::endl;
+
+    std::cout << "Press Enter to exit...";
+    std::cin.ignore();
+    std::cin.get();
 
     return 0;
 }
