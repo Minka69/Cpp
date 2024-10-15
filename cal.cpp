ce9e604d84c2f934b4adfa07cc37ad1952020011
Mark Shislov <115359051+Minka69@users.noreply.github.com>
2024-10-15 17:49:03 +0300

    Simple calculator

---
+++ cal.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char operation;
+    float num1, num2, result;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Enter an operation (+, -, *, /): ";
+    cin >> operation;
+    switch(operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '/':
+            if(num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operation!" << endl;
+            break;
+    }
+
+    return 0;
+}
