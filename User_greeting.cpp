39c67cdadac1e36c7726ad0c92cf62be921a3fe2
minka69 <marks.sislovs@gmail.com>
2024-10-30 14:13:18 +0200

    Added surname function

--- User_greeting.cpp
+++ User_greeting.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 #include <string>
-
+using namespace std;
 int main() {
-    std::string name;
+    string name;
     int age;
     int year;
+    string surname;
 
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
+    cout << "Enter your name: ";
+    getline(cin, name);
 
-    std::cout << "Enter your age: ";
-    std::cin >> age;
+    cout << "Enter your surname: ";
+    getline(cin, surname);
 
-    std::cout << "What year you were born in: ";
-    std::cin >> year;
+    cout << "Enter your age: ";
+    cin >> age;
 
-    std::cout << "Hello, " << name << "! You are " << age << " years old and you were born in " << year << "!" << std::endl;
+    cout << "What year you were born in: ";
+    cin >> year;
 
-    std::cout << "Press Enter to exit...";
-    std::cin.ignore();
-    std::cin.get();
+    cout << "Hello, " << name << " " << surname <<"!" " You are " << age << " years old and you were born in " << year << "!" << std::endl;
+
+
+    cout << "Press Enter to exit...";
+    cin.ignore();
+    cin.get();
 
     return 0;
 }
