0b8293f34fd195a9079ee4f60affe31ff7028ee2
minka69 <marks.sislovs@gmail.com>
2024-11-01 10:50:24 +0200

    Added: automatic age calculation, funny messages based on age, greeting based on the time of the day.

--- User_greeting.cpp
+++ User_greeting.cpp
@@ -1,26 +1,63 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
+
 int main() {
     string name;
-    int age;
-    int year;
     string surname;
+    int age;
+    char choice;
 
-    cout << "Enter your name: ";
-    getline(cin, name);
+    do {
+        cout << "Enter your name: ";
+        getline(cin, name);
 
-    cout << "Enter your surname: ";
-    getline(cin, surname);
+        cout << "Enter your surname: ";
+        getline(cin, surname);
 
-    cout << "Enter your age: ";
-    cin >> age;
+        cout << "Enter your age: ";
+        cin >> age;
 
-    cout << "What year you were born in: ";
-    cin >> year;
+        while (age <= 0 || age > 120) {
+            cout << "Please enter a valid age: ";
+            cin >> age;
+        }
 
-    cout << "Hello, " << name << " " << surname <<"!" " You are " << age << " years old and you were born in " << year << "!" << std::endl;
+        time_t now = time(0);
+        int currentYear = 1900 + localtime(&now)->tm_year;
+        int birthYear = currentYear - age;
 
+        int hour = localtime(&now)->tm_hour;
+        if (hour < 12) {
+            cout << "Good morning, ";
+        } else if (hour < 18) {
+            cout << "Good afternoon, ";
+        } else {
+            cout << "Good evening, ";
+        }
+
+        cout << name << " " << surname << "!" << endl;
+        cout << "---------------------------------\n";
+        cout << "Age: " << age << endl;
+        cout << "Year of Birth: " << birthYear << endl;
+
+        if (age < 18) {
+            cout << "Fun Fact: You're young and have a whole future ahead!\n";
+        } else if (age < 30) {
+            cout << "Fun Fact: The 20s are a great time to explore!\n";
+        } else if (age < 50) {
+            cout << "Fun Fact: You're in the prime of your life!\n";
+        } else {
+            cout << "Fun Fact: You're wise and experienced!\n";
+        }
+
+        cout << "---------------------------------\n";
+        cout << "Would you like to enter another profile? (y/n): ";
+        cin >> choice;
+        cin.ignore();
+
+    } while (choice == 'y' || choice == 'Y');
 
     cout << "Press Enter to exit...";
     cin.ignore();
